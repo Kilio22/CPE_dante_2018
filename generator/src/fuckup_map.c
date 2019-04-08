1e1a0b7073035e50dc1b7150dd7801b42e804acc
nathan.lecorchet@epitech.eu <nathan.lecorchet@epitech.eu@epitech.eu>
2019-04-08 13:12:00 +0200

    Improving perfect generation

--- generator/src/fuckup_map.c
+++ generator/src/fuckup_map.c
@@ -9,43 +9,27 @@
 #include <stdlib.h>
 #include "generator.h"
 
-static void remove_every_first_wall_line(char *map, long width)
+static void remove_wall(char *map, long i, long j, long w)
 {
-    long j = 0;
-
-    for (long i = 0; i < width; i++) {
-        j = 0;
-        while (j < width && map[MAP_NODE(i, j, width)] == '*')
-            j++;
-        if (j == width)
-            continue;
-        map[MAP_NODE(i, j, width)] = '*';
-    }
-}
-
-static void remove_every_first_wall_colmun(char *map, long height, long width)
-{
-    long i = 0;
-
-    for (long j = 0; j < width; j++) {
-        i = 0;
-        while (i < height && map[MAP_NODE(i, j, width)] == '*')
-            i++;
-        if (i == height)
-            continue;
-        map[MAP_NODE(i, j, width)] = '*';
-    }
+    if (map[MAP_NODE(i, j, w)] != 'X')
+        return;
+    if (map[MAP_NODE((i - 1), j, w)] != '*')
+        return;
+    if (map[MAP_NODE((i + 1), j, w)] != '*')
+        return;
+    if (map[MAP_NODE(i, (j - 1), w)] != 'X')
+        return;
+    if (map[MAP_NODE(i, (j + 1), w)] != 'X')
+        return;
+    map[MAP_NODE(i, j, w)] = '*';
 }
 
 void randomize_map_fuckup(char *map, long height, long width)
 {
-    long n = (height * width) / 2;
-    long x;
-    long y;
-
     if (!map)
         return;
-    while (n--) {
-        x = rand() % 2
+    for (long i = 1; i < height - 1; i++) {
+        for (long j = 1; j < width - 1; j++)
+            remove_wall(map, i, j, width);
     }
 }
