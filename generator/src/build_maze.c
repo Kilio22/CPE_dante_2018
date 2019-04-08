a63d892da36752a984fbdd281c77d43509ebe4a9
nathan.lecorchet@epitech.eu <nathan.lecorchet@epitech.eu@epitech.eu>
2019-04-08 11:47:17 +0200

    Reverting changes

--- generator/src/build_maze.c
+++ generator/src/build_maze.c
@@ -14,15 +14,32 @@ static void remove_child(struct node_s **nodes, long index, long child_nb)
         nodes[i] = nodes[i + 1];
 }
 
+static long add_child(struct map_s *map, struct node_s *origin, long child_nb)
+{
+    short direction_nb = -1;
+
+    direction_nb += origin->dir & NORTH;
+    direction_nb += origin->dir & EAST;
+    direction_nb += origin->dir & WEST;
+    direction_nb += origin->dir & SOUTH;
+    if (direction_nb < 1)
+        return child_nb;
+    for (short i = 0; i < direction_nb && child_nb < MAX_CHILDS; i++)
+        map->nodes[child_nb++] = link_node(map, origin);
+    return child_nb;
+}
+
 long build_maze(struct map_s *map, struct node_s *start, long child_nb)
 {
-    // long child_pop_ratio = 4;
+    long child_pop_ratio = 5;
 
     for (long i = 0; i < child_nb; i++) {
         if (map->nodes[i] == start) {
             remove_child(map->nodes, i--, child_nb--);
             continue;
         }
+        if (child_nb < MAX_CHILDS && !(rand() % child_pop_ratio))
+            child_nb = add_child(map, map->nodes[i], child_nb);
         map->nodes[i] = link_node(map, map->nodes[i]);
     }
     return child_nb;
